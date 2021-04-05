798d545d1837147de995209bc1555498b3b4620c
pedRoot <pedrotorres@outlook.com>
2021-04-05 18:56:43 -0400

    FEATURE: Change port for app

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 
 const host = '0.0.0.0'
-const port = process.env.port || 3000
+const port = process.env.port || 80
 const env = app.get('env')
 
 app.listen(host, port, () => console.log(`App server listen on ${host} port: ${port}, for ${env}`))

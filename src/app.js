4bf1484cd0b04da97caacf5adf6f3ebfc9ac22e8
pedRoot <pedrotorres@outlook.com>
2021-04-05 18:24:05 -0400

    ADD: Permite la carga de datos de configuración

--- src/app.js
+++ src/app.js
@@ -6,10 +6,10 @@ import pkg from '../package.json'
 const app = express()
 
 import * as run from "./libs/setupScripts";
-// run.createRole();
-// run.createMeeting();
-// run.createDraw();
-// run.createUser();
+run.createRole();
+run.createMeeting();
+run.createDraw();
+run.createUser();
 
 app.set('pkg', pkg)
 
@@ -17,7 +17,6 @@ app.use(morgan('dev'))
 app.use(express.json())
 
 // routes
-//import productRoutes from './routes/product.routes'
 import authRoutes from './routes/auth.routes'
 import userRoutes from './routes/user.routes'
 import drawRoutes from './routes/draw.routes'
@@ -32,7 +31,6 @@ app.get('/', (req, res) => {
   console.log('Epale chamo esto está yendo bien...')
 })
 
-// app.use('/api/v1/products', productRoutes)
 app.use('/api/v1/auth', authRoutes)
 app.use('/api/v1/users', userRoutes)
 app.use('/api/v1/draws', drawRoutes)

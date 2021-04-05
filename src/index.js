5c795e345295cfe6a468d9925f4c4d8fc09ae91b
pedRoot <pedrotorres@outlook.com>
2021-04-05 19:16:50 -0400

    FEATURE: Cambia número de puerto

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './config/database';
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-const port = process.env.port || 80
+const port = process.env.port || 3580
 const env = app.get('env')
 
 app.listen(port, () => console.log(`App server listen on port: ${port}, for ${env}`))

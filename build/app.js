22a89c9b0b22044717bfcd89bb834f5a2b97b2b0
pedRoot <pedrotorres@outlook.com>
2021-04-09 18:01:14 -0400

    ADD: Integration whit sentry

--- build/app.js
+++ build/app.js
@@ -1,5 +1,21 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+require("core-js/modules/es.weak-map.js");
+
+require("core-js/modules/es.object.to-string.js");
+
+require("core-js/modules/es.string.iterator.js");
+
+require("core-js/modules/es.array.iterator.js");
+
+require("core-js/modules/web.dom-collections.iterator.js");
+
+require("core-js/modules/es.object.get-own-property-descriptor.js");
+
+require("core-js/modules/es.symbol.iterator.js");
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -17,21 +33,43 @@ var _morgan = _interopRequireDefault(require("morgan"));
 
 var _package = _interopRequireDefault(require("../package.json"));
 
+var Sentry = _interopRequireWildcard(require("@sentry/node"));
+
+var Tracing = _interopRequireWildcard(require("@sentry/tracing"));
+
+var run = _interopRequireWildcard(require("./libs/setupScripts"));
+
 var _auth = _interopRequireDefault(require("./routes/auth.route"));
 
 var _user = _interopRequireDefault(require("./routes/user.route"));
 
 var _draw = _interopRequireDefault(require("./routes/draw.route"));
 
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// Configuring the app
-var app = (0, _express.default)(); // import * as run from "./libs/setupScripts";
-// run.createRole();
-// run.createMeeting();
-// run.createDraw();
-// run.createUser();
-
+var app = (0, _express.default)();
+run.createRole();
+run.createMeeting();
+run.createDraw();
+run.createUser();
+Sentry.init({
+  dns: "https://1d155a81c4c148bca3b44ef3bf6d6700@o568415.ingest.sentry.io/5713468",
+  integrations: [new Sentry.Integrations.Http({
+    tracing: true
+  }), new Tracing.Integrations.Express({
+    app: app
+  })],
+  tracesSampleRate: 1.0
+});
+app.use(Sentry.Handlers.requestHandler({
+  serverName: false,
+  user: ["email"]
+}));
+app.use(Sentry.Handlers.tracingHandler());
 app.set('pkg', _package.default);
 app.use((0, _morgan.default)('dev'));
 app.use(_express.default.json()); // routes
@@ -48,5 +86,10 @@ app.get('/', function (req, res) {
 app.use('/api/v1/auth', _auth.default);
 app.use('/api/v1/users', _user.default);
 app.use('/api/v1/draws', _draw.default);
+app.use(Sentry.Handlers.errorHandler());
+app.use(function onError(err, req, res, next) {
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
 var _default = app;
 exports.default = _default;
--- src/app.js
+++ src/app.js
@@ -1,15 +1,31 @@
-// Configuring the app
 import express from 'express'
 import morgan from 'morgan'
 import pkg from '../package.json'
+import * as Sentry from "@sentry/node"
+import * as Tracing from "@sentry/tracing"
 
 const app = express()
 
-// import * as run from "./libs/setupScripts";
-// run.createRole();
-// run.createMeeting();
-// run.createDraw();
-// run.createUser();
+import * as run from "./libs/setupScripts";
+run.createRole();
+run.createMeeting();
+run.createDraw();
+run.createUser();
+
+Sentry.init({
+  dns: "https://1d155a81c4c148bca3b44ef3bf6d6700@o568415.ingest.sentry.io/5713468",
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+})
+
+app.use(Sentry.Handlers.requestHandler({
+  serverName: false,
+  user: ["email"],
+}))
+app.use(Sentry.Handlers.tracingHandler())
 
 app.set('pkg', pkg)
 
@@ -34,5 +50,12 @@ app.get('/', (req, res) => {
 app.use('/api/v1/auth', authRoutes)
 app.use('/api/v1/users', userRoutes)
 app.use('/api/v1/draws', drawRoutes)
+
+app.use(Sentry.Handlers.errorHandler())
+
+app.use(function onError(err, req, res, next) {
+  res.statusCode = 500
+  res.end(res.sentry + "\n")
+})
 
 export default app;

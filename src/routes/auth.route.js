93f35db8f497dd787cde40f30f8551212ae7625e
pedRoot <pedrotorres@outlook.com>
2021-04-30 08:45:13 -0400

    ADD: Pruebas de configuraación de CORS

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,12 +1,13 @@
 import * as authController from "../controllers/auth.controller"
 import {Router} from "express"
 import {authJWT} from "../middlewares"
-import cors from "cors"
-import corsOptions from "../config/cors"
+// import cors from "cors"
+// import corsOptions from "../config/cors"
 
 const router = Router()
 
 router.post('/signup', [authJWT.verifyToken, authJWT.isAdmin], authController.signup)
-router.get('/signin', cors(corsOptions), authController.signin)
+// router.get('/signin', cors(corsOptions), authController.signin)
+router.get('/signin', authController.signin)
 
 export default router;

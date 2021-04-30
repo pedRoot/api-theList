479e850bd8ccbd2e77fa00acac058a6c86c9f1e7
pedRoot <pedrotorres@outlook.com>
2021-04-29 22:14:33 -0400

    HOTFIX: Corrige error en llaves

--- src/config/cors.js
+++ src/config/cors.js
@@ -2,15 +2,15 @@ import conf from "./config-env"
 
 const whileList = [conf.URL_FRONT]
 
-const corsOptions = {
+export default corsOptions = {
   origin: function (origin, callback) {
     if (whileList.indexOf(origin) !== -1 || !origin) {
       callback(null, true)
     } else {
-    if (whileList.indexOf(origin) !== -1 || !origin) {
-      callback(new Error(`Not allowed by CORS: ${origin} || ${conf.URL_FRONT}..`))
+      if (whileList.indexOf(origin) !== -1 || !origin) {
+        const msgError = `Not allowed by CORS: ${origin} || ${conf.URL_FRONT}..`;
+        callback(new Error(msgError))
+      }
     }
-  }
+  }  
 }
-
-export default corsOptions;
